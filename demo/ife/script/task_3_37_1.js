bc28f0f9568b59dfd9de0935c1368f24a9fd1091
hollowtree <smsmer@outlook.com>
2016-04-11 18:46:00 +0800

    update

--- demo/ife/script/task_3_37_1.js
+++ demo/ife/script/task_3_37_1.js
@@ -1,8 +1,7 @@
 window.onload = function () {
-    var bigBox = document.getElementById("background-box");
     var backgroundBox = document.getElementById("background-box");
     var bigBoxHeight = window.innerHeight + "px";
-    bigBox.style.height = bigBoxHeight;
+    backgroundBox.style.height = bigBoxHeight;
     var closeBtn = document.getElementById("close-btn");
     closeBtn.onclick = function () {
         backgroundBox.style.display = "none";
