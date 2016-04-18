5d9d0f70e2645058d06931731f55ff3f0330fe28
hollowtree <smsmer@outlook.com>
2016-04-18 09:47:48 +0800

    update

--- demo/ife/script/task_2_16_1.js
+++ demo/ife/script/task_2_16_1.js
@@ -1,78 +1,84 @@
-/**
- * aqiData，存储用户输入的空气指数数据
- * 示例格式：
- * aqiData = {
- *    "北京": 90,
- *    "上海": 40
- * };
- */
+function g(id) {
+    return document.getElementById(id);
+}
+
 var aqiData = {};
 
-/**
- * 从用户输入中获取数据，向aqiData中增加一条数据
- * 然后渲染aqi-list列表，增加新增的数据
- */
-function addAqiData() {
-    var btn = document.getElementById("add-btn");
-    var cityName = document.getElementById("aqi-city-input").value;
-    var airValue = document.getElementById("aqi-value-input").value;
-    aqiData[cityName] = airValue;
-}
-
-/**
- * 渲染aqi-table表格
- */
-function renderAqiList() {
-    var myTable = document.getElementById("aqi-table").getElementsByTagName("tbody")[0];
-    var dtdThr = document.createElement("td");
-    for (cityName in aqiData) {
-        var definition = aqiData[cityName];
-        var dtr = document.createElement("tr");
-        var dtdOne = document.createElement("td");
-        var dtdTwo = document.createElement("td");
-
-        var dtdTextOne = document.createTextNode(cityName);
-        dtdOne.appendChild(dtdTextOne);
-        dtr.appendChild(dtdOne);
-        var dtdTextTwo = document.createTextNode(aqiData[cityName]);
-        dtdTwo.appendChild(dtdTextTwo);
-        dtr.appendChild(dtdTwo);
-        dtr.appendChild(dtdThr);
-        dtdThr.innerHTML = '<button>删除</button>';
+//检测输入城市名是否合法
+g("aqi-city-input").onblur = function () {
+    var cityInput = g("aqi-city-input").value.trim();
+    if (cityInput == "") {
+        g("tip1").innerHTML = "输入不能为空!";
+        g("tip1").style.color = "red";
+    } else if (/[^a-z\u4E00-\u9FA5]+/gi.test(cityInput)) {
+        g("tip1").innerHTML = "输入不合法! 城市名只能包括中文英文";
+        g("tip1").style.color = "red";
+    } else {
+        g("tip1").innerHTML = "OK!";
+        g("tip1").style.color = "black";
     }
-    myTable.appendChild(dtr);
+}
+//检测输入的空气质量指数是否合法
+g("aqi-value-input").onblur = function () {
+    var valueInput = g("aqi-value-input").value.trim();
+    if (valueInput == "") {
+        g("tip2").innerHTML = "输入不能为空!";
+        g("tip2").style.color = "red";
+    } else if (/[^0-9]+/gi.test(valueInput)) {
+        g("tip2").innerHTML = "输入不合法!质量指数只能为整数";
+        g("tip2").style.color = "red";
+    } else {
+        g("tip2").innerHTML = "OK!";
+        g("tip2").style.color = "black";
+    }
+}
+//存储输入的数据
+function addAqiData() {
+    var cityInput = g("aqi-city-input").value.trim();
+    var valueInput = g("aqi-value-input").value.trim();
+
+    if (cityInput && valueInput) {
+        aqiData[cityInput] = valueInput;
+    }
 }
 
-/**
- * 点击add-btn时的处理逻辑
- * 获取用户输入，更新数据，并进行页面呈现的更新
- */
-function addBtnHandle() {
+//渲染aqi-table表格
+function renderAqiList() {
+    g("aqi-table").innerHTML = "<tr><th>城市</th><th>空气质量</th><th>操作</th></tr>";
+
+    for (cityInput in aqiData) {
+
+        var tableTr = document.createElement("tr");
+        var tableTd1 = document.createElement("td");
+        var tableTd2 = document.createElement("td");
+        var tableTd3 = document.createElement("td");
+
+        var tableTd1Text = document.createTextNode(cityInput);
+        tableTd1.appendChild(tableTd1Text);
+
+        var tableTd2Text = document.createTextNode(aqiData[cityInput]);
+        tableTd2.appendChild(tableTd2Text);
+
+        var tableTd3Btn = document.createElement("button");
+        var tableTd3BtnText = document.createTextNode("删除");
+        tableTd3Btn.appendChild(tableTd3BtnText);
+        tableTd3Btn.setAttribute("data-city", cityInput);
+        tableTd3Btn.className = "del-btn";
+        tableTd3.appendChild(tableTd3Btn);
+
+        tableTr.appendChild(tableTd1);
+        tableTr.appendChild(tableTd2);
+        tableTr.appendChild(tableTd3);
+        g("aqi-table").appendChild(tableTr);
+    }
+}
+//删除选中的数据
+g("aqi-table").onclick = function (event) {
+    delete aqiData[event.target.getAttribute("data-city")];
+    renderAqiList();
+}
+
+g("add-btn").onclick = function () {
     addAqiData();
     renderAqiList();
 }
-
-/**
- * 点击各个删除按钮的时候的处理逻辑
- * 获取哪个城市数据被删，删除数据，更新表格显示
- */
-function delBtnHandle() {
-    // do sth.
-    alert("9");
-    renderAqiList();
-}
-
-
- function init() {
-
-    // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-    var addBtn = document.getElementById("add-btn");
-    addBtn.onclick = addBtnHandle;
-    // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-    var delBtn = document.getElementById("aqi-table").getElementsByTagName("button");
-    for (var i = 0; i < delBtn.length; i++) {
-        delBtn[i].onclick = delBtnHandle;
-    }
-}
-
-init();
--- menu.js
+++ menu.js
@@ -1,20 +1,32 @@
 var arr = new Array();
 arr = [
     ['单页面制作',
-        ['demo/ife/task_1_01_1.html', 'HTML页面'],
-        ['demo/ife/task_1_02_1.html', 'HTML+CSS页面1'],
+        //['demo/ife/task_1_01_1.html', 'HTML页面'],
         ['demo/ife/task_1_03_1.html', '三栏式布局'],
         ['demo/ife/task_1_04_1.html', '定位及居中'],
-        ['demo/ife/task_1_05_1.html', 'HTML+CSS页面2'],
+        ['demo/ife/task_1_08_1.html', '响应式网格布局'],
+        ['demo/ife/task_1_10_1.html', 'Flexbox布局'],
+        ['demo/ife/task_1_12_1.html', '一些CSS3新特性'],
+        ['demo/ife/task_1_02_1.html', 'HTML+CSS页面Ⅰ'],
+        ['demo/ife/task_1_05_1.html', 'HTML+CSS页面Ⅱ'],
+        ['demo/ife/task_1_11_1.html', '移动Web页面'],
         ['demo/ife/task_1_06_1.html', '模拟报纸排版'],
-        // ['demo/ife/task_1_07_1.html', '常见的页面架构及样式布局'],
 
     ],
     ['网页小部件',
         ['demo/component/101/index.html', '简易电子时钟'],
         ['demo/component/201/index.html', '绘制七巧板'],
+        ['demo/ife/task_3_37_1.html', '浮出层'],
+        ['demo/ife/task_3_38_1.html', '排序表格'],
+        ['demo/ife/task_3_39_1.html', '首行冻结的表格'],
+        ['demo/ife/task_3_40_1.html', '日历组件Ⅰ'],
+        ['demo/ife/task_3_41_1.html', '日历组件Ⅱ'],
+        ['demo/ife/task_3_42_1.html', '日历组件Ⅲ'],
+
+
+        //['',''],
         //['#', '谷歌'],
-        //['#', '微博']
+        //['#', '微博'],
     ]
 ];
 
@@ -25,9 +37,7 @@ for (var i = 0; i < arr.length; i++) {
             list += '<div class="myMenu"><dt class="fMenu">' + arr[i][0] + '</dt><div class="sMenu">';
         } else {
             list += '<dd><a href="' + arr[i][j][0] + '" target="_blank">' + j + ")  " + arr[i][j][1] + '</a></dd>';
-
         }
-
     }
     list += '</div></div>';
 }
