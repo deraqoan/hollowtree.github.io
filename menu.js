5b80a00f84bbb8ac06d11283ebaaa8cc0d97a20d
hollowtree <smsmer@outlook.com>
2016-05-16 00:14:27 +0800

    首页更新

--- menu.js
+++ menu.js
@@ -1,4 +1,4 @@
-var arr = new Array();
+﻿var arr = new Array();
 arr = [
     ['单页面制作',
         //['demo/ife/task_1_01_1.html', 'HTML页面'],
@@ -7,12 +7,13 @@ arr = [
         ['demo/ife/task_1_08_1.html', '响应式网格布局'],
         ['demo/ife/task_1_10_1.html', 'Flexbox布局'],
         ['demo/ife/task_1_12_1.html', '一些CSS3新特性'],
+        ['demo/component/301/svg.html', 'SVG 初试'],
         ['demo/ife/task_1_02_1.html', 'HTML+CSS页面Ⅰ'],
         ['demo/ife/task_1_05_1.html', 'HTML+CSS页面Ⅱ'],
-        ['demo/ife/task_1_11_1.html', '移动Web页面'],
+        ['demo/pages/bootstrap/index.html', 'Bootstrap 初试'],
+        ['demo/ife/task_1_11_1.html', '自适应移动Web页面'],
         ['demo/ife/task_1_06_1.html', '模拟报纸排版'],
-        ['demo/ife/task_1_09_1.html', '一个复杂页面'],
-
+        ['demo/ife/task_1_09_1.html', '一个复杂页面']
     ],
     ['网页小部件',
         ['demo/component/101/index.html', '简易电子时钟'],
@@ -23,14 +24,13 @@ arr = [
         ['demo/algorithms/sort-bubble.html', '冒泡排序可视化'],
         ['demo/ife/task_3_40_1.html', '日历组件Ⅰ'],
         ['demo/ife/task_3_41_1.html', '日历组件Ⅱ'],
-        ['demo/ife/task_3_42_1.html', '日历组件Ⅲ'],
+        ['demo/ife/task_3_42_1.html', '日历组件Ⅲ']
 
 
         //['',''],
         //['#', '谷歌'],
     ]
 ];
-
 var list = "";
 for (var i = 0; i < arr.length; i++) {
     for (var j = 0; j < arr[i].length; j++) {
